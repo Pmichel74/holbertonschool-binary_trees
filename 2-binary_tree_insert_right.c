84348e71b0de65a90cefb5de4c35062fa8b6be18
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-09 15:41:08 +0100

    fix struct task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,12 +2,12 @@
 
 /**
  * binary_tree_insert_right - Insert a node as the right-child
- *                            of another in a binary tree.
+ * of another in a binary tree.
  * @parent: A pointer to the node to insert the right-child in.
  * @value: The value to store in the new node.
  *
  * Return: If parent is NULL or an error occurs - NULL.
- *         Otherwise - a pointer to the new node.
+
  *
  * Description: If parent already has a right-child, the new node
  *              takes its place and the old right-child is set as
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new = binary_tree_node(parent, value);
+	new = binary_tree_t(parent, value);
 	if (new == NULL)
 		return (NULL);
 
