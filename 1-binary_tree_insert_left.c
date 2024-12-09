e9a224a9ea9896963da3e0eb86de4afca20fbb6c
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-09 15:43:10 +0100

    fix struct task 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,13 +14,12 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-
 	binary_tree_t *new;
 
 	if (!parent)
 	return (NULL);
 
-	new = binary_tree_node(parent, value);
+	new = binary_tree_t(parent, value);
 
 	if (!new)
 	return (NULL);
