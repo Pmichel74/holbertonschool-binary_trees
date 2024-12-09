207f2c139bd12c290828d9801c7c0b82e174b06e
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-09 20:12:39 +0100

    fix condition task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,13 +8,9 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaf = 0;/* compteur de feuilles*/
-
-	if (tree)
-		/*ajoute 1 si c une feuille sinon 0*/
-	{	leaf += (!tree->left && !tree->right) ? 1 : 0;
-		leaf += binary_tree_leaves(tree->left);/*ajoute feuilles dans sous arbre L*/
-		leaf += binary_tree_leaves(tree->right);/*ajoute feuilles dans sous arbre R*/
-	}
-	return (leaf);
+		if (!tree)
+			return (0);
+		if (!tree->left && !tree->right)
+			return (1);
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
