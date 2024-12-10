9b0eb7e463326a877fae9394e07c37eeb5a2a977
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-10 18:44:02 +0100

    push task16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,24 +8,16 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-
-	size_t left;
-	size_t right;
+	size_t left = 0, right = 0;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
-	else
-	{
-		left = binary_tree_height(tree->left) + 1;
-		right = binary_tree_height(tree->right) + 1;
+	if (tree->left)
+		left = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right = 1 + binary_tree_height(tree->right);
 
-		if (left > right)
-			return (left);
-		else
-			return (right);
-	}
+	return (left > right ? left : right);
 }
 
 /**
