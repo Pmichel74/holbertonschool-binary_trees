978a7661eef94cfef8a7e7bd8cff69dd52936afb
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-10 15:59:09 +0100

    push task11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include <stdio.h>
 
 /**
  * binary_tree_height - Measures the height of a binary tree
@@ -11,7 +10,7 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 if (tree == NULL)
-return(0);
+return (0);
 
 return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 
