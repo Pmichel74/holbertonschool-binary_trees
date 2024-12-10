065070bbcb96fffce133763e564ed04a10b32b95
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-10 10:20:30 +0100

    push task 14

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,6 +13,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->parent != NULL)
+	/*ajoute 1 et continue recursivement vers le haut*/
 		return (binary_tree_depth(tree->parent) + 1);
 	else
 		return (0);
--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,27 @@
 #include "binary_trees.h"
 
 /**
+ * _binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: If tree is NULL, your function must return 0, else return height.
+ */
+size_t _binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+
+	if (!tree)
+		return (0);
+
+	/* si noeud actuel a un enfant droite,*/
+	/* ajoute 1 a la hauteur avec recursivité */
+	right = tree->right ? 1 + _binary_tree_height(tree->right) : 0;
+	left = tree->left ? 1 + _binary_tree_height(tree->left) : 0;
+
+	return (left > right ? left : right);
+}
+
+/**
  * binary_tree_balance - Measures the balance factor of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the balance factor.
  *
@@ -8,5 +29,8 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
 
+		return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
 }
