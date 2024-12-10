8b5be18131539fa7c10d45c043e5c9ca119f39b6
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-10 10:29:58 +0100

    fix syntax error

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,7 +8,7 @@
  */
 size_t _binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left = 0, right = 0;
+	size_t left, right;
 
 	if (!tree)
 		return (0);
@@ -18,7 +18,7 @@ size_t _binary_tree_height(const binary_tree_t *tree)
 	right = tree->right ? 1 + _binary_tree_height(tree->right) : 0;
 	left = tree->left ? 1 + _binary_tree_height(tree->left) : 0;
 
-	return (left > right ? left : right);
+	return (left >= right ? left : right);
 }
 
 /**
